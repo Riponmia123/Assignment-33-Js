a7b2cfe56bb670f2a6acfa1aec5628c15fe464b7
Ripon Mia <125129675+Riponmia123@users.noreply.github.com>
2023-03-27 08:47:22 +0600

    first js code practice

---
+++ app.js
@@ -0,0 +1,41 @@
+// let a = 30;
+// let b = 20;
+// a += b;
+// console.log(a);
+
+
+// let a = 30;
+// let b = 10;
+// a -= b;
+// console.log(a);
+
+// let x = 10;
+// let y = 50;
+// x *= y
+// console.log(x);
+
+// let a = 5;
+// let b = 10;
+// b /= a;
+// console.log(b);
+
+// let a = 5;
+// let b = 12;
+// b %= a;
+// console.log(b);
+
+// let a = 50;
+// let b = 40;
+// let sum = (a + b);
+// console.log(sum);
+
+// let a = 50;
+// let b = 40;
+// let sum = (a - b);
+// console.log(sum);
+
+// let a = 50;
+// let b = 40;
+// let sum = (a * b);
+// console.log(sum);
+
